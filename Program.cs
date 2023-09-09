3531145c06100010edcee418ccfe73a485b77df4
phamnam91th <phamnam1991@gmail.com>
2023-09-09 14:23:22 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+using NumberToText;
+
+Console.WriteLine("Hello, World!");
+string s = Utils.NumberToText(3456);
+Console.WriteLine(s);
---
+++ Utils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NumberToText
+{
+    internal class Utils
+    {
+        public static string NumberToText(int num)
+        {
+            char[] number = new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
+            string[] text = new string[] { "khong", "mot", "hai", "ba", "bon", "nam", "sau", "bay", "tam", "chin"};
+            string str = num.ToString();
+            StringBuilder sb = new StringBuilder(str);
+            char[] chars = str.ToCharArray();
+            
+            for (int i = 0; i<str.Length; i++)
+            {
+
+            }
+
+
+
+
+            return str;
+        }
+    }
+}
